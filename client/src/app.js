87ad6e7497fdd5be0de2c9282426eb911f2ff1e7
434980373@qq.com <434980373@qq.com>
2021-04-02 00:03:58 +0800

    自定义icon库

--- client/src/app.js
+++ client/src/app.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react'
-import Taro from '@tarojs/taro'
-import 'taro-ui/dist/style/index.scss'
-import './app.less'
-// import './icon.scss'
+import React, { Component } from "react";
+import Taro from "@tarojs/taro";
+import "taro-ui/dist/style/index.scss";
+import "./app.less";
+import "./icon.scss";
 
 class App extends Component {
-
-  componentDidMount () {
-    if (process.env.TARO_ENV === 'weapp') {
-      Taro.cloud.init()
+  componentDidMount() {
+    if (process.env.TARO_ENV === "weapp") {
+      Taro.cloud.init();
     }
   }
 
-  componentDidShow () {}
+  componentDidShow() {}
 
-  componentDidHide () {}
+  componentDidHide() {}
 
-  componentDidCatchError () {}
+  componentDidCatchError() {}
 
   // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
+  render() {
+    return this.props.children;
   }
 }
 
-export default App
+export default App;
